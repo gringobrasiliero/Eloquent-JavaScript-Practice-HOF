fd43ced8533f2a9f83c3a633225bfafe07eb1059
Nolan Byrnes <nolanehs@gmail.com>
2018-09-14 07:01:23 -0400

    Completed JS exercise from assessment

---
+++ filterEmails.js
@@ -0,0 +1,21 @@
+const customers = [{name: 'Ian', email: 'ian.candy@flatironschool.com'}, {name: 'Nolan'}, {name: 'Alice', email: false}, {name: 'Cernan', email: null}]
+
+function withEmail(array) {
+  let passed = []
+  for(let element of array) {
+    if (element.email) {
+      passed.push(element.name)
+    }
+  }
+  console.log(passed)
+}
+
+withEmail(customers) //['Ian']
+
+
+// Grab customers
+// Iterate through each customers
+// check for email
+// if email === truthy
+// append customer into email Array
+// return email Array
