ae6083213c25fa604a57e0381a4b575acaddce90
Nolan Byrnes <nolanehs@gmail.com>
2018-09-16 10:38:06 -0400

    rewrote in es6

--- exercise1.js
+++ exercise1.js
@@ -1,8 +1,10 @@
 let arrays = [[1, 2, 3], [4, 5], [6]];
 // Your code here.
 // → [1, 2, 3, 4, 5, 6]
-var result = arrays.reduce(function (previous, current){
-  return previous.concat(current)
-})
+// var result = arrays.reduce(function (previous, current){
+//   return previous.concat(current)
+// })
+
+var result = arrays.reduce((previous, current) => previous.concat(current))
 
 console.log(result)
